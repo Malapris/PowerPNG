1785c1afdfb81057464fe76af7174e4fdae2fcf6
Malapris <123722969+Malapris@users.noreply.github.com>
2023-01-27 14:28:08 +0100

    update v1.1

--- PowerPNG.bat
+++ PowerPNG.bat
@@ -1,13 +1,15 @@
 @echo off
-echo PowerPNG multithreaded
+echo PowerPNG V1.1 (multithreaded) by Francis Malapris : github.com/Malapris/PowerPNG
 IF "%~1" == "" GOTO :NOPARAM
 set "folder=%~1"
 set "p=%~d0%~p0"
 powershell -file "%p%PowerPNG.ps1" -folder "%folder%" -threads 5
+echo.
+echo If you like this software, please consider buying be a coffee : buymeacoffee.com/malapris
 pause
 exit
 
 :NOPARAM
-echo Sorry ! No directory provided for the scan. Please drop a directory on the icon of PowerPNG.
+echo Sorry ! No directory provided for the scan. Please drop a directory on the nice multicolor icon of PowerPNG.bat
 pause
 exit
---
+++ README.txt
@@ -0,0 +1,12 @@
+PowerPNG v1
+
+Multithreaded utility to recompress recursively PNG files, based on PNGQUANT.
+
+Features : 
+- Simple : just drop a folder on the shortcut and the program will find PNG files
+- Fast ! The program is multithreaded with 5 simultaneous compressions
+- 60% Gain ! For files generated with A1111
+
+
+PNGQUANT original and updated software is available here : 
+https://pngquant.org/
